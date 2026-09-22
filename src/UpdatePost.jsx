ba43b0c33165569d8bc3e0bed64743aff195dab6
adeeltahirdev <adeeltahir0301@gmail.com>
2026-09-22 13:13:43 +0500

    Implemented PUT request

--- src/UpdatePost.jsx
+++ src/UpdatePost.jsx
@@ -5,24 +5,56 @@ function UpdatePost() {
 
     const {id} = useParams()
 
-    const [post, setPost] = useState(null)
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
     
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then((response) => response.json())
-        .then((data) => setPost(data))
+        .then((data) => {
+            setTitle(data.title)
+            setBody(data.body)
+        })
         .catch((error) => console.log('Error fetching post: ', error))
     }, [id])
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 1,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+    }
+
     return(
-        <div className="edit-page">
+        <div className="container">
             <h1>Edit Post</h1>
-                        {post && (
-                <>
-                    <h1>{post.title}</h1>
-                    <p>{post.body}</p>
-                </>
-            )}
+                <form onSubmit={handleSubmit} className="post-card">
+                    <label className="label">Title:</label>
+                    <input
+                        className="title"
+                        type="text"
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                    <label className="label">Body:</label>
+                    <textarea
+                        className="text-area"
+                        value={body}
+                        onChange={(event) => setBody(event.target.value)}
+                    ></textarea>
+                    <button type="submit" className="submit-btn">Update</button>
+                </form>
         </div>
     );
 }
